336eb6bc29d1ce504d20493cff026374b6661552
rilsonalmeida <rilson.almeida@gmail.com>
2021-03-05 20:55:31 -0300

    Final version

---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+import Navigation from "@components/navigation"
+
+export default function Contact() {
+    return (
+        <>
+            <Navigation />
+            <header>
+                <h2>Hello from contact page!</h2>
+            </header>
+        </>
+    )
+}
